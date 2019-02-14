4984a689197f021f2765a769258579f758ffafb0
puskar <bistpuskar123@gmail.com>
2019-02-14 07:28:54 +0545

    first commit

---
+++ application/controllers/Users.php
@@ -0,0 +1,60 @@
+<?php
+
+class Users extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->model('users_model');
+	}
+
+	public function index(){
+		$data['users'] = $this->users_model->getAllUsers();
+		$this->load->view('user_list', $data);
+	}
+
+	public function addnew(){
+		$this->load->view('addform');
+	}
+
+	public function insert(){
+		$user['username'] = $this->input->post('username');
+		$user['password'] = $this->input->post('password');
+		$user['fname'] = $this->input->post('fname');
+		
+		$query = $this->users_model->insertuser($user);
+
+		if($query){
+			header('location:'.base_url().$this->index());
+		}
+
+	}
+
+	public function edit($id){
+		$data['user'] = $this->users_model->getUser($id);
+		$this->load->view('editform', $data);
+	}
+
+	public function update($id){
+		$user['username'] = $this->input->post('username');
+		$user['password'] = $this->input->post('password');
+		$user['fname'] = $this->input->post('fname');
+
+		$query = $this->users_model->updateuser($user, $id);
+
+		if($query){
+			header('location:'.base_url().$this->index());
+		}
+	}
+
+	public function delete($id){
+		$query = $this->users_model->deleteuser($id);
+
+		if($query){
+			header('location:'.base_url().$this->index());
+		}
+	}
+}
+
+
+?>
